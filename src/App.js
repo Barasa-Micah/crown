588199cd72dbace8c57ba0b2af84e7002584943e
Barasa-Micah <barasamica@gmail.com>
2024-07-11 01:16:46 +0300

    Installed the react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 
 function App() {
   return (
+   <Router>
     <div className="App">
-      <LoginPage />
+      <Switch>
+        <Route path="/signup">
+          <SignupPage />
+        </Route>
+        <Route path="/">
+        <LoginPage />
+        </Route>
+      </Switch>
     </div>
+   </Router>
   );
 }
 
